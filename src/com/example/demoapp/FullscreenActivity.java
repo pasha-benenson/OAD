f5e3addb44866a86c3d6a436f01cddb40a69f98d
pasha-benenson <pasha.benenson@gmail.com>
2013-08-23 09:18:34 -0400

    first commit

---
+++ src/com/example/demoapp/FullscreenActivity.java
@@ -0,0 +1,264 @@
+package com.example.demoapp;
+
+import com.example.demoapp.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+//import android.content.Context;
+import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+//import android.preference.PreferenceManager;
+import android.view.MotionEvent;
+import android.view.View;
+//import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * 
+ * @see SystemUiHider
+ */
+public class FullscreenActivity extends Activity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+	 * user interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = true;
+
+	/**
+	 * The flags to pass to {@link SystemUiHider#getInstance}.
+	 */
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+	/**
+	 * The instance of the {@link SystemUiHider} for this activity.
+	 */
+	private SystemUiHider mSystemUiHider;
+//	private TextView myText;
+	private String text = "Not Authenticated";
+
+	private static String function_name="";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_fullscreen);
+
+
+		Intent intent = getIntent();
+		System.out.println(intent);
+		OAuthHelper oh = new OAuthHelper(this);
+		
+		String action = intent.getAction();
+		if (action.equals(Intent.ACTION_VIEW)) {
+			String data = intent.getDataString();
+
+			System.out.println(data);
+			
+			oh.setRefreshToken(data); 
+			text = "Refresh Token is \n" + OAuthHelper.RefreshToken;
+			if (function_name=="time"){
+				function_name="";
+				text = oh.callapi("?method=time");
+				if (!text.isEmpty())
+					text=(OAuthHelper.fromJSONtoTable(text));
+
+			}
+			if (function_name=="id"){
+				function_name="";
+				text = oh.callapi("?method=id");
+				if (!text.isEmpty())
+					text=(OAuthHelper.fromJSONtoTable(text));
+
+			}
+			
+			
+		}
+		{
+
+			final View controlsView = findViewById(R.id.fullscreen_content_controls);
+			final View contentView = findViewById(R.id.fullscreen_content);
+			TextView t = (TextView) contentView;
+			t.setText(text);
+			// Set up an instance of SystemUiHider to control the system UI for
+			// this activity.
+			mSystemUiHider = SystemUiHider.getInstance(this, contentView,
+					HIDER_FLAGS);
+			mSystemUiHider.setup();
+			mSystemUiHider
+					.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+						// Cached values.
+						int mControlsHeight;
+						int mShortAnimTime;
+
+						@Override
+						@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+						public void onVisibilityChange(boolean visible) {
+							if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+								// If the ViewPropertyAnimator API is available
+								// (Honeycomb MR2 and later), use it to animate
+								// the
+								// in-layout UI controls at the bottom of the
+								// screen.
+								if (mControlsHeight == 0) {
+									mControlsHeight = controlsView.getHeight();
+								}
+								if (mShortAnimTime == 0) {
+									mShortAnimTime = getResources()
+											.getInteger(
+													android.R.integer.config_shortAnimTime);
+								}
+								controlsView
+										.animate()
+										.translationY(
+												visible ? 0 : mControlsHeight)
+										.setDuration(mShortAnimTime);
+							} else {
+								// If the ViewPropertyAnimator APIs aren't
+								// available, simply show or hide the in-layout
+								// UI
+								// controls.
+								controlsView
+										.setVisibility(visible ? View.VISIBLE
+												: View.GONE);
+							}
+
+							if (visible && AUTO_HIDE) {
+								// Schedule a hide().
+								delayedHide(AUTO_HIDE_DELAY_MILLIS);
+							}
+						}
+					});
+
+			// Set up the user interaction to manually show or hide the system
+			// UI.
+			contentView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					if (TOGGLE_ON_CLICK) {
+						mSystemUiHider.toggle();
+					} else {
+						mSystemUiHider.show();
+					}
+				}
+			});
+
+			// Upon interacting with UI controls, delay any scheduled hide()
+			// operations to prevent the jarring behavior of controls going away
+			// while interacting with the UI.
+			// findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+		}
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		// Trigger the initial hide() shortly after the activity has been
+		// created, to briefly hint to the user that UI controls
+		// are available.
+		// pb delayedHide(100);
+	}
+
+	/**
+	 * Touch listener to use for in-layout UI controls to delay hiding the
+	 * system UI. This is to prevent the jarring behavior of controls going away
+	 * while interacting with activity UI.
+	 */
+	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (AUTO_HIDE) {
+				delayedHide(AUTO_HIDE_DELAY_MILLIS);
+			}
+			return false;
+		}
+	};
+
+	Handler mHideHandler = new Handler();
+	Runnable mHideRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mSystemUiHider.hide();
+		}
+	};
+
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide(int delayMillis) {
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+	}
+
+	/** Called when the user clicks the Send button */
+	public void click_auth(View view) {
+		// Do something in response to button
+		OAuthHelper oh = new OAuthHelper(this);
+		oh.authenticate();
+
+		// System.out.println(uriUrl);
+	}
+
+	public void click_clear(View view) {
+		// Do something in response to button
+		//OAuthHelper oh = new OAuthHelper(this);
+		OAuthHelper.clear();
+
+//		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		final View contentView = findViewById(R.id.fullscreen_content);
+		TextView t = (TextView) contentView;
+		t.setText("clear");
+
+		// System.out.println(uriUrl);
+	}
+
+	public void click_callapi_id(View view) {
+		// Do something in response to button
+		function_name="id";
+//		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		final View contentView = findViewById(R.id.fullscreen_content);
+		TextView t = (TextView) contentView;
+		
+		t.setText("");
+		OAuthHelper oh = new OAuthHelper(this);
+		String text = oh.callapi("?method=id");
+		if (!text.isEmpty())
+			t.setText(OAuthHelper.fromJSONtoTable(text));
+		// System.out.println(uriUrl);
+	}
+		public void click_callapi_time(View view) {
+			// Do something in response to button
+			function_name="time";
+//			final View controlsView = findViewById(R.id.fullscreen_content_controls);
+			final View contentView = findViewById(R.id.fullscreen_content);
+			TextView t = (TextView) contentView;
+			
+			t.setText("");
+			OAuthHelper oh = new OAuthHelper(this);
+			String text = oh.callapi("?method=time");
+			if (!text.isEmpty())
+				t.setText(OAuthHelper.fromJSONtoTable(text));
+			// System.out.println(uriUrl);
+
+	}
+}
---
+++ src/com/example/demoapp/OAuthHelper.java
@@ -0,0 +1,356 @@
+package com.example.demoapp;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+//import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+//import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.text.format.Time;
+//import android.content.*;
+
+public class OAuthHelper {
+	public static String RefreshToken="";
+	public static String AccessToken="";
+	public static final String PREFS_NAME = "OAuth_demo_PREF_NAME";
+	private static Activity activity;
+
+
+	static Time expires=new Time();
+	public OAuthHelper(Activity ac) {
+		activity=ac;
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy); 
+		
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+				// TODO Auto-generated method stub
+
+			}
+		} };
+
+		try {
+			SSLContext sc = SSLContext.getInstance("SSL");
+
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+			HttpsURLConnection
+					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (KeyManagementException kme) {
+			kme.printStackTrace();
+		} catch (NoSuchAlgorithmException nsae) {
+			nsae.printStackTrace();
+		}
+	
+
+	}
+	public  String setRefreshToken(String data){
+		String rtoken="";
+		String code=data.substring( data.indexOf("?code=")+6);
+		System.out.println(code);
+		
+		URL hurl;
+		try {
+			hurl = new URL("https://sso.pbens.com:9031/as/token.oauth2");
+	
+			HostnameVerifier v = new HostnameVerifier() {
+							@Override
+				public boolean verify(String arg0, SSLSession arg1) {
+					// TODO Auto-generated method stub
+					return true;
+				}
+			};
+	
+           
+            
+		
+			HttpsURLConnection https = (HttpsURLConnection) hurl.openConnection();
+			https.setHostnameVerifier(v);
+			https.setReadTimeout(10000);
+			https.setConnectTimeout(15000);
+			https.setRequestMethod("POST");
+			https.setDoInput(true);
+			https.setDoOutput(true);
+
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("code", code));
+			params.add(new BasicNameValuePair("grant_type", "authorization_code"));
+			params.add(new BasicNameValuePair("client_id", "mobile_client"));
+			
+
+			OutputStream os = https.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(
+			        new OutputStreamWriter(os, "UTF-8"));
+			writer.write(getQuery(params));
+			writer.close();
+			os.close();
+		    System.out.println(getQuery(params));
+			https.connect();
+		      int status = https.getResponseCode();
+
+		    System.out.println("status code= " + status);
+		        
+		    BufferedReader br = new BufferedReader(new InputStreamReader(https.getInputStream()));
+		    StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			br.close();
+			String rdata=sb.toString();	
+			System.out.println("response data: "+ rdata);
+			JSONObject json = new JSONObject(rdata);
+			System.out.println(json);
+			RefreshToken=json.getString("refresh_token");
+			
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
+			Editor edit = preferences.edit();
+            edit.putString("RefreshToken", RefreshToken);
+            
+            edit.commit();
+
+			
+			AccessToken=json.getString("access_token");
+			String expires_in=json.getString("expires_in");
+			Time t = new Time();
+			
+			expires=new Time();
+			expires.set(t.toMillis(false)+Integer.parseInt(expires_in)*1000);
+			
+			System.out.println("Refresh Token:" + RefreshToken);
+			System.out.println("Access Token" + AccessToken);
+			System.out.print("time now "); System.out.println(t);
+			System.out.print("expires "); System.out.println(expires);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rtoken;
+	}
+	public   String getRefreshToken(){
+		System.out.println("getRefreshToken "+ RefreshToken );
+
+		if (RefreshToken==""){
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
+		    RefreshToken = preferences.getString("RefreshToken","");
+		    System.out.println("got token from storage "+ RefreshToken );
+		    if (RefreshToken==""){
+		    	
+		    	authenticate();
+		    }
+		
+		}
+		
+            return RefreshToken;
+		
+	}
+	private static String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException
+	{
+	    StringBuilder result = new StringBuilder();
+	    boolean first = true;
+
+	    for (NameValuePair pair : params)
+	    {
+	        if (first)
+	            first = false;
+	        else
+	            result.append("&");
+
+	        result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
+	        result.append("=");
+	        result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+	    }
+
+	    return result.toString();
+	}
+	public  String callapi(String queryparameter){
+		URL hurl;
+		try {
+			hurl = new URL("http://pbens.com/api/index.php"+ queryparameter);
+	
+		
+			
+           
+            
+		
+			HttpURLConnection http =  (HttpURLConnection) hurl.openConnection();
+			
+			http.setReadTimeout(10000);
+			http.setConnectTimeout(15000);
+			http.setRequestMethod("POST");
+			http.setDoInput(true);
+			http.setDoOutput(true);
+            http.setRequestProperty("Authorization", "Bearer " + getAccessToken());
+
+			http.connect();
+		      int status = http.getResponseCode();
+
+		    System.out.println("status code= " + status);
+		        
+		    BufferedReader br = new BufferedReader(new InputStreamReader(http.getInputStream()));
+		    StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			br.close();
+			String rdata=sb.toString();	
+			System.out.println("response data: "+ rdata);
+			JSONObject json = new JSONObject(rdata);
+			System.out.println(json);
+
+		return json.getString("data");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+		
+	}
+
+	private  String getAccessToken() {
+		Time t = new Time();
+		t.setToNow();
+		System.out.println("getAccessToken "+ AccessToken + expires.toString());
+		if (AccessToken!="" && expires.after(t))
+			return AccessToken;
+		else{
+			RefreshToken=getRefreshToken();
+			// use refresh token to get access token
+			URL hurl;
+			try {
+				hurl = new URL("https://sso.pbens.com:9031/as/token.oauth2");
+		
+				HostnameVerifier v = new HostnameVerifier() {
+								@Override
+					public boolean verify(String arg0, SSLSession arg1) {
+						// TODO Auto-generated method stub
+						return true;
+					}
+				};
+	           
+	            
+			
+				HttpsURLConnection https = (HttpsURLConnection) hurl.openConnection();
+				https.setHostnameVerifier(v);
+				https.setReadTimeout(10000);
+				https.setConnectTimeout(15000);
+				https.setRequestMethod("POST");
+				https.setDoInput(true);
+				https.setDoOutput(true);
+
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("refresh_token", RefreshToken));
+				params.add(new BasicNameValuePair("grant_type", "refresh_token"));
+				params.add(new BasicNameValuePair("client_id", "mobile_client"));
+
+				OutputStream os = https.getOutputStream();
+				BufferedWriter writer = new BufferedWriter(
+				        new OutputStreamWriter(os, "UTF-8"));
+				writer.write(getQuery(params));
+				writer.close();
+				os.close();
+				
+				 System.out.println(getQuery(params));
+				 
+				https.connect();
+			      int status = https.getResponseCode();
+
+			    System.out.println("status code= " + status);
+			        
+			    BufferedReader br = new BufferedReader(new InputStreamReader(https.getInputStream()));
+			    StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = br.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+				br.close();
+				String rdata=sb.toString();	
+				System.out.println("response data: "+ rdata);
+				JSONObject json = new JSONObject(rdata);
+				System.out.println(json);
+
+				
+				AccessToken=json.getString("access_token");
+				String expires_in=json.getString("expires_in");
+				
+				
+				
+				expires=new Time();
+				expires.set(t.toMillis(false)+Integer.parseInt(expires_in)*1000);
+				
+
+				System.out.println("Refresh Token:" + RefreshToken);
+				System.out.println("Access Token" + AccessToken);
+				System.out.print("time now "); System.out.println(t);
+				System.out.print("expires "); System.out.println(expires);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return AccessToken;
+		}
+	}
+	public void authenticate(){
+		System.out.println("about to  authenticate" );
+		Time time=new Time();
+		time.setToNow();
+		
+    	Uri uriUrl = Uri.parse("https://sso.pbens.com:9031/as/authorization.oauth2?client_id=mobile_client&response_type=code&pfidpadapterid=Form1&time=" + time.toString());
+		Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+		activity.startActivity(launchBrowser);
+	}
+	public static void clear(){
+		RefreshToken="";
+		AccessToken="";
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
+		Editor edit = preferences.edit();
+        edit.putString("RefreshToken", RefreshToken);
+        
+        edit.commit();	
+
+	}
+	public static String fromJSONtoTable(String sjson){
+		
+		return sjson.replace(",", ",\n");
+	}
+	}
